68b735b79aaa29a503a6243588b3678bd31742ef
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-07 18:51:11 +0600

    Firebase Auth Functions Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,10 @@ import { useAuth } from "../../API/useAuth";
 import Title from "../../Components/Shared/Title/Title";
 
 const Home = () => {
-  const { user } = useAuth();
-
   return (
     <>
       <Title title="Home" />
-      <div>Home form {user.name}</div>
+      <div>Home form </div>
     </>
   );
 };
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,11 +1,52 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const user = { name: "rabby" };
-  const authInfo = { user };
+  const [userLoading, setUserLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
 
+  const createUser = (email, password) => {
+    setUserLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (name, image) => {
+    setUserLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: image,
+    });
+  };
+
+  const userSignin = (email, password) => {
+    setUserLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUserLoading(false);
+      setUser(currentUser);
+    });
+  }, []);
+
+  const authInfo = {
+    user,
+    userLoading,
+    createUser,
+    updateUserProfile,
+    userSignin,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
