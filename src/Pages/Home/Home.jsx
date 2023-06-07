919cc7d5dc24a22ce81baf35c70fcfd2a9dde892
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-07 18:35:26 +0600

    Title Component Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useAuth } from "../../API/useAuth";
+import Title from "../../Components/Shared/Title/Title";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { user } = useAuth();
+
+  return (
+    <>
+      <Title title="Home" />
+      <div>Home form {user.name}</div>
+    </>
+  );
 };
 
 export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./Routes/Routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./Providers/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={routes} />
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
